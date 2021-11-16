02e953db72325d680df207e303d7301d3746bebc
fatih <fatihenaz@gmail.com>
2021-11-16 23:17:01 +0300

    created comparison page

--- src/components/Comparison.js
+++ src/components/Comparison.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Comparison = () => {
+const Comparison = ({id}) => {
     return (
         <div>
             Comparison
+        
+        
         </div>
     )
 }
