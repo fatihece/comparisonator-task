b4bf99aa380d157fec0da5fb3f01255443482fce
fatih <fatihenaz@gmail.com>
2021-11-16 22:03:05 +0300

    created stats of players

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
+// import { useParams } from "react-router";
 import axios from "axios";
-import { Container, Row, Col } from "react-bootstrap";
-import { Link, useRouteMatch } from "react-router-dom";
+// import { Container, Row, Col } from "react-bootstrap";
+// import { Link, useRouteMatch } from "react-router-dom";
 
 const Stats = ({ id }) => {
   const [stats, setStats] = useState([]);
@@ -17,17 +17,53 @@ const Stats = ({ id }) => {
   );
 
   return (
-    <div>
+    <div className="stats-container">
       {loading && <div>Loading...</div>}
-      <p>{stats.goals}</p>
+      {/* <p>{stats.goals}</p>
       <p>{stats.assists}</p>
-      <p>{stats.passes}</p>
-          
-        <div>
-              {
-                  
-              }
-        </div>
+      <p>{stats.passes}</p> */}
+      <div>
+      progress bar
+      </div>
+
+      <div className="column">
+        <ul style={{textDecoration:"none"}}>
+          <li style={{textDecoration:"none"}}>Goals</li>
+          <li>Asists</li>
+          <li>Shots</li>
+          <li>Passes</li>
+          <li>Crosses</li>
+        </ul>
+        <ul>
+        {Object.values(stats).slice(1, 6)
+                        .map((item) => {
+                          return <li><strong>{item}</strong></li>;
+                        })}
+        </ul>
+      </div>
+
+      <div className="column">
+        <ul>
+          <li>Key Passes</li>
+          <li>Smart Passes</li>
+          <li>Touch in Box</li>
+          <li style={{color:"yellow"}}>Yellow Cards</li>
+          <li style={{color:"red"}}>Red Cards</li>
+        </ul>
+        <ul>
+        {Object.values(stats).slice(1, 6)
+                        .map((item) => {
+                            return <li><strong>{item}</strong></li>;
+                        })}
+        </ul>
+      </div>
+
+
+
+
+
+
+
     </div>
   );
 };
